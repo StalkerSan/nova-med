a7dbfdb3f78270bb6897b7bc82dcd858c97aa721
armankuanysh <33935708+armankuanysh@users.noreply.github.com>
2019-09-18 23:12:26 +0600

    feat(main-page markup)

--- src/js/MainPage.js
+++ src/js/MainPage.js
@@ -3,10 +3,17 @@ import Swiper from 'swiper';
 export default function MainPage() {
   const HeroSection = document.querySelector('.hero');
   if(HeroSection){
-    console.log('hi');
     
     const HeroSlider = new Swiper('.hero__slider', {
       slidesPerView: 1
+    })
+
+    const FeedsSider = new Swiper('.feeds__slider--init', {
+      slidesPerView: 3,
+      navigation: {
+        prevEl: '.feeds__slider--prev',
+        nextEl: '.feeds__slider--next'
+      }
     })
   }
 }
