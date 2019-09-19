0a9376ea988bce8e25ab2b5324f7d06219658a2d
armankuanysh <33935708+armankuanysh@users.noreply.github.com>
2019-09-19 23:20:36 +0600

    feat(main page & footer markup)

--- src/js/MainPage.js
+++ src/js/MainPage.js
@@ -6,14 +6,41 @@ export default function MainPage() {
     
     const HeroSlider = new Swiper('.hero__slider', {
       slidesPerView: 1
-    })
+    });
 
-    const FeedsSider = new Swiper('.feeds__slider--init', {
+    const FeedsSlider = new Swiper('.feeds__slider--init', {
       slidesPerView: 3,
       navigation: {
         prevEl: '.feeds__slider--prev',
         nextEl: '.feeds__slider--next'
       }
-    })
+    });
+
+    const ExpertsSlider = new Swiper('.experts__slider--init', {
+      slidesPerView: 3,
+      navigation: {
+        prevEl: '.experts__slider--prev',
+        nextEl: '.experts__slider--next'
+      }
+    });
+
+    const FAQDropdown = () => {
+      const Trigger = document.querySelectorAll('.faq__item--top');
+      const Content = document.querySelectorAll('.faq__item');
+      Trigger.forEach((it, i) => {
+        let isOpen = false;
+        it.addEventListener('click', () => {
+          if(!isOpen){
+            Content[i].classList.add('is-open');
+            isOpen = true;
+          } else {
+            Content[i].classList.remove('is-open');
+            isOpen = false;
+          }
+        })
+      });
+    }
+
+    FAQDropdown();
   }
 }
