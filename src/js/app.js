bf816026f9be6abf182de394f03d47de34a62637
Arman Kuanysh <imac@Arman-Kuanysh.local>
2019-10-22 11:12:01 +0600

    feat(cities modal)

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,42 @@
-import fancybox from '@fancyapps/fancybox';
-import MainPage from './MainPage';
-import ExpertSingle from './ExpertSingle';
-import About from './About';
-import Questions from './Questions';
+import fancybox from "@fancyapps/fancybox";
+import MainPage from "./MainPage";
+import ExpertSingle from "./ExpertSingle";
+import About from "./About";
+import Questions from "./Questions";
 
-const brg = document.querySelector('.header__brg');
-const nav = document.querySelector('.header__nav');
+const brg = document.querySelector(".header__brg");
+const nav = document.querySelector(".header__nav");
 let isOpen = false;
-brg.addEventListener('click', ()=>{
-  if(!isOpen){
-    nav.classList.add('is-active');
+brg.addEventListener("click", () => {
+  if (!isOpen) {
+    nav.classList.add("is-active");
     isOpen = true;
   } else {
-    nav.classList.remove('is-active');
+    nav.classList.remove("is-active");
     isOpen = false;
   }
-})
+});
 
-const wipTrigger = document.querySelectorAll('.wip__trigger');
-const wipModal = document.querySelector('.wip__modal--video iframe');
+const wipTrigger = document.querySelectorAll(".wip__trigger");
+const wipModal = document.querySelector(".wip__modal--video iframe");
 wipTrigger.forEach(it => {
-  it.addEventListener('click', () => {
-    const src = it.getAttribute('data-video-url');
-    console.log("🐞: src", src)
-    wipModal.setAttribute('src', src);
+  it.addEventListener("click", () => {
+    const src = it.getAttribute("data-video-url");
+    console.log("🐞: src", src);
+    wipModal.setAttribute("src", src);
     $.fancybox.open({
-      src: '#wip__modal--video',
-      type: 'inline'
-    })
-  })
-})
+      src: "#wip__modal--video",
+      type: "inline"
+    });
+  });
+});
+
+window.openCityModal = () => {
+  $.fancybox.open({
+    src: "#citiesModal",
+    type: "inline"
+  });
+};
 
 MainPage();
 ExpertSingle();
