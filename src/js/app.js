08d676f6de48f6184f9883bdd6993a06b5ad0069
Arman Kuanysh <imac@Arman-Kuanysh.local>
2019-10-24 20:18:16 +0600

    feat(countup numbers)

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import fancybox from "@fancyapps/fancybox";
+import { CountUp } from "./lib/countUp";
 import MainPage from "./MainPage";
 import ExpertSingle from "./ExpertSingle";
 import About from "./About";
@@ -37,6 +38,17 @@ window.openCityModal = () => {
     type: "inline"
   });
 };
+$(document).ready(() => {
+  let turned = false;
+  $(window).scroll(() => {
+    const st = $(window).scrollTop();
+    if (st >= 800 && !turned) {
+      const coutup1 = new CountUp("countup1", 320000).start();
+      const coutup2 = new CountUp("countup2", 150000).start();
+      turned = true;
+    }
+  });
+});
 
 MainPage();
 ExpertSingle();
